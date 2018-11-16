3daac7e9239ae918bc60b7a1f5ec9a3845c82dcc
arun967 <arunvenkatesh96@gmail.com>
2018-11-16 14:08:11 +0530

    MavenExpert

---
+++ src/main/java/com/controller/AddServlet.java
@@ -0,0 +1,51 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public AddServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter(); 
+		
+		String n1 = request.getParameter("no1");
+		 String n2 = request.getParameter("no2");
+		 int no1 = Integer.parseInt(n1);
+		 int no2 = Integer.parseInt(n2);
+		 
+		 int ans = no1+no2; 
+		 
+		 out.println("Answer is : "+ans);
+		 
+		 
+	}
+
+}
---
+++ src/main/webapp/Addition.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Addition</title>
+</head>
+<body>
+  <form action ="AddServlet" method ="post">
+  Enter number one<input type="text" name = "no1" ><br>
+  Enter number two<input type="text" name = "no2" ><br>
+  <input type = "submit" value = "add">
+  </form>
+
+</body>
